5b77da28c668a616a9b616b9bd4c348c97d95347
Paul Tarvydas <guitarvydas@gmail.com>
2022-03-29 05:35:23 -0400

    "found" and "answer" nil in lookup.lisp

--- lookup.lisp
+++ lookup.lisp
@@ -4,8 +4,9 @@
   (let ((prototypes (list *lookup* *scroll-through-atoms* *match-single-atom-name* *unsuccessful* *successful*)))
     (let ((top-context1 (instantiate *lookup* nil prototypes)))
       (mem-reset mem)
-      (let ((top-context ($maybe-set-field top-context1 '$args `( (junk . nil) (name . ,name) (atom-memory . ,mem) 
-								  (found . nil) (answer . nil) ))))
+      (let ((top-context ($maybe-set-field top-context1 '$args `( (junk . nil) (name . ,name) (atom-memory . ,mem) ))))
+	($!local top-context "found" nil)
+	($!local top-context "answer" nil)
         ($dispatch top-context)))))
 
 
